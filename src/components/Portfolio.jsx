13c3db2f31237855e45b40cc472a82f733180dc2
M-Dearlove <dearlove.matthew@gmail.com>
2025-03-15 22:28:47 -0400

    bug fix.

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PageLayout from "./PageLayout";
-import '../styles/Portfolio.css';
+import '../styles/portfolio.css';
+
 export default function Portfolio() {
     const projects = [
         {
